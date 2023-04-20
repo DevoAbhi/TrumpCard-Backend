b96551a030e585eef3afcb8bf3a93dcb68373082
AbhiDroid <abhinabroy2001@gmail.com>
2023-04-21 04:45:09 +0530

    add a auth middleware

---
+++ middleware/auth.js
@@ -0,0 +1,21 @@
+import jwt from 'jsonwebtoken';
+
+const auth = async (req, res, next) => {
+    try {
+        const token = req.headers.Authorization.split(" ")[1];
+
+        let decodedData;
+
+        if(token) {
+            decodedData = jwt.verify(token, "very-long-secret-jwt-token");
+
+            req.userId = decodedData?.id;
+        }
+
+        next();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export default auth;
