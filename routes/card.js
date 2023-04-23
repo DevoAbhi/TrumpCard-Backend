ac6fa4e647c7f423932d0fbb2e7a1fb5489d27df
AbhiDroid <abhinabroy2001@gmail.com>
2023-04-23 19:58:09 +0530

    add auth middleware in card route

--- routes/card.js
+++ routes/card.js
@@ -2,8 +2,9 @@ import express from 'express';
 const router = express.Router();
 
 import {postCardDeckHandler} from '../controllers/card.js';
+import authMiddleware from '../middleware/auth.js';
 
-router.post('/get-card', postCardDeckHandler);
+router.post('/get-card', authMiddleware, postCardDeckHandler);
 
 export default router;
 
