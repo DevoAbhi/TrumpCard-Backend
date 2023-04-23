ddeb3d36625e92a3b9786dd0d320c365272a9f63
AbhiDroid <abhinabroy2001@gmail.com>
2023-04-23 19:55:42 +0530

    add delete card collection function

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,7 +8,7 @@ export const postSignUp = async (req, res, next) => {
     try {
         const { username, email, password } = req.body;
 
-        if(!(email && password && username)) {
+        if (!(email && password && username)) {
             return res.status(400).json({
                 success: false,
                 message: "Please send all the details"
@@ -18,15 +18,15 @@ export const postSignUp = async (req, res, next) => {
 
 
         try {
-            const existingUser = await User.findOne({email});
-            if(existingUser) {
+            const existingUser = await User.findOne({ email });
+            if (existingUser) {
                 return res.status(200).json({
                     success: true,
                     message: "User is already registered"
                 })
             }
         }
-        catch(err){
+        catch (err) {
             console.log(err);
             return res.status(500).json({
                 success: false,
@@ -51,7 +51,7 @@ export const postSignUp = async (req, res, next) => {
         });
 
 
-        const token = jwt.sign({email: user.email, id: user._id}, "very-long-secret-jwt-token", {expiresIn: '2hr'});
+        const token = jwt.sign({ email: user.email, id: user._id }, "very-long-secret-jwt-token", { expiresIn: '2hr' });
         user.password = undefined;
 
         console.log("User registered");
@@ -75,32 +75,33 @@ export const postSignUp = async (req, res, next) => {
 
 export const postLogin = async (req, res, next) => {
 
-    try{
+    try {
         const { email, password } = req.body;
-        if(!(email && password)) {
+        if (!(email && password)) {
             return res.status(400).json({
                 success: false,
                 message: "Please send all the details"
             })
         }
 
-        const existingUser = await User.findOne({email});
-        if(existingUser) {
+        const existingUser = await User.findOne({ email });
+        if (!existingUser) {
             return res.status(400).json({
                 success: false,
-                message: "User already exists!"
+                message: "User does not exists!"
             })
         }
 
-        const isPasswordCorrect = await bcrypt.comapare(password, existingUser.password);
-        if(!isPasswordCorrect) {
+        const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
+        if (!isPasswordCorrect) {
             return res.status(400).json({
                 success: false,
                 message: "Password entered is incorrect!"
             })
         }
 
-        const token = jwt.sign({email: existingUser.email, id: existingUser._id}, "very-long-secret-jwt-token", {expiresIn: '2hr'});
+        const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, "very-long-secret-jwt-token", { expiresIn: '2hr' });
+        console.log("User logged in")
 
         return res.status(200).json({
             success: true,
@@ -109,17 +110,66 @@ export const postLogin = async (req, res, next) => {
             token
         })
     }
-    catch(err) {
+    catch (err) {
         return res.status(500).json({
             success: false,
             message: "Something went wrong!"
         })
     }
 
+}
+
+export const postDeleteCardCollection = async (req, res, next) => {
+    try {
+        if (!req.userId) {
+            return res.status(400).json({
+                success: false,
+                message: "User is not logged in!"
+            })
+        }
+
+        const { cardType } = req.body;
+        console.log(cardType);
+
+        const user = await User.findOne({ _id: req.userId });
+        if (!user) {
+            return res.status(400).json({
+                success: false,
+                message: "Wrong user token passed!"
+            })
+        }
+
+        let cardCollection = user.cardCollection;
+        cardCollection = cardCollection.filter((card) => card.cardType !== cardType);
+
+        console.log(cardCollection);
 
 
+        const result = await User.findOneAndUpdate(
+            { _id: req.userId },
+            { $set: { cardCollection: cardCollection } }
+        )
 
+        if (!result) {
+            return res.status(500).json({
+                success: false,
+                message: "Something went wrong updating the user"
+            })
+        }
 
+        console.log("User card collection updated", result);
+        return res.status(200).json({
+            success: true,
+            message: "User card collection has been updated"
+        })
 
+    }
+
+    catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
 
 }
