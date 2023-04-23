20f74e1470f80aed8336056bbcd41621563fa35a
AbhiDroid <abhinabroy2001@gmail.com>
2023-04-23 19:57:38 +0530

    add auth middleware in auth route for deletion from card collection

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,11 @@
 import express from "express";
 const router = express.Router();
 
-import { postSignUp, postLogin } from "../controllers/auth.js";
+import { postSignUp, postLogin, postDeleteCardCollection } from "../controllers/auth.js";
+import authMiddleware from '../middleware/auth.js';
 
 router.post('/signup', postSignUp);
 router.post('/login', postLogin);
+router.post('/delete-card', authMiddleware, postDeleteCardCollection);
 
 export default router;
